d429569c182d49a87b9cb463a84c923d58df4ff6
Cafel Ong <caferatte89@hotmail.com>
2019-11-16 01:43:01 +0800

    123

--- Assignment PCD2 2019/Header.h
+++ Assignment PCD2 2019/Header.h
@@ -1,9 +1,35 @@
 #pragma once
 
+// Self-defined function.
+void StaffInformation();
+void Facility();
+void UserInformation();
+void Booking();
+void FacilityUsage();
+
 // For date usage.
 typedef struct {
 	int day, month, year;
 } Date;
+
+// For user infomation usage.
+typedef struct {
+	char name[32];
+	char gender[3], IC[16];
+	char phoneNum[13];
+} UserInfo;
+
+// For staff information usage.
+typedef struct {
+	char staffID[7];
+	UserInfo info;
+} StaffInfo;
+
+// For student information usage.
+typedef struct {
+	char studentID[12];
+	UserInfo info;
+} StudentInfo;
 
 // For password XOR usage?
 #define XOR1 0b01100010
--- Assignment PCD2 2019/Main.c
+++ Assignment PCD2 2019/Main.c
@@ -7,14 +7,59 @@ Facility Usage Module - Chin Sze Sing
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "Header.h"
 #pragma warning(disable:4996)
 
+void loginScreen();
+
 int main3()
 {
-	printf("先放着");
+	int selection = -1;
+	char valid;
 
-	system("pause");
-	return 0;
+	void loginScreen();
+	while (1)
+	{
+		printf("\n1. Facility detail\n");
+		printf("2. Checking facility usage\n");
+		printf("3. Booking a facility\n");
+		printf("4. Register\n");
+		printf("5. Staff related perform\n");
+		printf("6. Exit");
+
+		// Let user to choose a selection.
+		printf("Choose a selection to proceed: ");
+		rewind(stdin);
+		scanf("%d%c", &selection, &valid);
+
+		while (selection < 1 || selection > 6 || valid != '\n')
+		{
+			printf("Invalid input! Please try again: ");
+			selection = -1;
+			rewind(stdin);
+			scanf("%d%c", &selection, &valid);
+		}
+
+		switch (selection)
+		{
+		case 1:
+			//Facility(); break;
+		case 2:
+			//FacilityUsage(); break;
+		case 3:
+			//Booking(); break;
+		case 4:
+			//UserInformation(); break;
+		case 5:
+			//StaffInformation();
+			break;
+		case 6:
+			return 0;
+		}
+	}
+}
+
+void loginScreen()
+{
+
 }
